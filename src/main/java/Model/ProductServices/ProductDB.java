ceca133869c0ddd298de3da36b0ad08ba48c6493
Lyapustin Nikita <pasandep@yandex.ru>
2020-11-04 21:57:31 +0000

    Revert "Revert "Добавил рейтинг. Надеюсь последний коммит. (pull request #12)" (pull request #13)"
    This reverts pull request #13.
    
    > This reverts pull request #12.
    > 
    > > Добавил рейтинг. Надеюсь последний коммит.
    > 
    > ‌

--- src/main/java/Model/ProductServices/ProductDB.java
+++ src/main/java/Model/ProductServices/ProductDB.java
@@ -12,7 +12,9 @@ public class ProductDB implements ProductDAO {
     private static MySQLConnection connection = new MySQLConnection();
 
     private static final String SQL_SELECT_PRODUCT_BY_ID
-            = "SELECT id, `name`, description, company, image, price, count_of_mark, rating FROM NNGamingShopDB.products where id = ?";
+            = "SELECT id, `name`, description, company, image, price, count_of_marks, rating FROM products JOIN `product-rating` ON product_id = id WHERE id = ?";
+
+    private static final String SQL_CHANGE_RATING_BY_PRODUCT_ID = "UPDATE `product-rating` SET count_of_marks = ?, rating = ? WHERE product_id = ?";
 
     public Product getProductByID(int id) {
         try(Connection conn = connection.getNewConnection()){
@@ -39,11 +41,36 @@ public class ProductDB implements ProductDAO {
             product.setImage(resultSet.getString("image"));
             product.setCompany(resultSet.getString("company"));
             product.setPrice(resultSet.getDouble("price"));
-            product.setCountOfMark(resultSet.getInt("count_of_mark"));
+            product.setCountOfMark(resultSet.getInt("count_of_marks"));
             product.setRating(resultSet.getDouble("rating"));
         }
 
         return product;
     }
 
+    public boolean setMark(int mark, int productId) throws SQLException {
+        try(Connection conn = connection.getNewConnection()) {
+            try (PreparedStatement preparedStatement = conn.prepareStatement(SQL_SELECT_PRODUCT_BY_ID)) {
+                preparedStatement.setInt(1, productId);
+
+                Product product = getProductFromDB(preparedStatement);
+
+                System.out.println("___");
+                System.out.println(product);
+                System.out.println(product.getId());
+                System.out.println("---");
+                double currentRating = product.getRating();
+                int currentCountOfMark = product.getCountOfMark();
+
+                try (PreparedStatement preparedStatementForRating = conn.prepareStatement(SQL_CHANGE_RATING_BY_PRODUCT_ID)) {
+                    preparedStatementForRating.setInt(1, currentCountOfMark + 1);
+                    preparedStatementForRating.setDouble(2, (currentRating*currentCountOfMark + mark)/(currentCountOfMark + 1));
+                    preparedStatementForRating.setInt(3, productId);
+                    preparedStatementForRating.executeUpdate();
+                }
+
+                return true;
+            }
+        }
+    }
 }
